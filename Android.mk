191a7f8d4059cfe9fcf481a43746fa53bdb6469c
ghbhaha <ghbhaha@gmail.com>
2016-11-05 19:26:56 +0800

    update sudasdk
    Change-Id: I5d270123f601831fa342e0e08ac8687e01945a8f

--- Android.mk
+++ Android.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libSudaSdk:libSudaSdk.jar \
-	libOkHttp:libOkHttp.jar \
-	libOKIO:libOKIO.jar \
 	libSudaPinYin:libSudaPinYin.jar
 include $(BUILD_MULTI_PREBUILT)
 
