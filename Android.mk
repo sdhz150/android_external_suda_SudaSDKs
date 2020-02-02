f35102b94131b44a1d922ca0e10abbb87523085e
sdhz150 <yanghui140@gmail.com>
2020-02-02 12:40:29 +0800

    Update

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libSudaSdk:libSudaSdk.jar \
-	libSudaPinYin:libSudaPinYin.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libSudaPinYin:libSudaPinYin.jar
 include $(BUILD_MULTI_PREBUILT)
 
